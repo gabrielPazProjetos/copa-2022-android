df9ffa61b60a45856cb75a1ca51e114ec8457910
Gabriel Paz <gabrieltecncontato@gmail.com>
2025-11-03 00:55:50 -0300

    Update EnableNotificationUseCase.kt

--- domain/src/main/kotlin/me/dio/copa/catar/domain/usecase/EnableNotificationUseCase.kt
+++ domain/src/main/kotlin/me/dio/copa/catar/domain/usecase/EnableNotificationUseCase.kt
@@ -1,12 +1,32 @@
 package me.dio.copa.catar.domain.usecase
 
+import android.content.Context
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.workDataOf
+import dagger.hilt.android.qualifiers.ApplicationContext
+import me.dio.copa.catar.domain.model.MatchDomain
 import me.dio.copa.catar.domain.repositories.MatchesRepository
+import me.dio.copa.catar.worker.NotificationMatcherWorker
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class EnableNotificationUseCase @Inject constructor(
-    private val repository: MatchesRepository
+    private val repository: MatchesRepository,
+    private val workManager: WorkManager,
+    @ApplicationContext private val context: Context
 ) {
-    suspend operator fun invoke(id: String) {
-        repository.enableNotificationFor(id)
+    suspend operator fun invoke(match: MatchDomain) {
+  
+        repository.enableNotificationFor(match.id)
+
+        val data = workDataOf("matchName" to match.name)
+
+        val request = OneTimeWorkRequestBuilder<NotificationMatcherWorker>()
+            .setInputData(data)
+            .setInitialDelay(10, TimeUnit.SECONDS) 
+            .build()
+
+        workManager.enqueue(request)
     }
 }
